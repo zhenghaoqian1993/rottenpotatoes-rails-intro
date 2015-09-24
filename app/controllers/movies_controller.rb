315de38e8e6448f296d76807638e67e19f5743ec
zhenghaoqian <zhenghaoqian1993@gmail.com>
2015-09-23 22:34:09 -0500

    first commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,5 @@
 class MoviesController < ApplicationController
-
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
-  end
+  helper_method :ratings_filter, :sort_column, :sort_direction
 
   def show
     id = params[:id] # retrieve movie ID from URI route
@@ -11,7 +8,29 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    redirect = false
+    if params[:ratings] != ratings_filter
+      redirect = true
+    end
+    if params[:sort] != sort_column
+      redirect = true
+    end
+
+    if session[:ratings] != ratings_filter
+      session[:ratings] = ratings_filter
+    end
+    if session[:sort] != sort_column
+      session[:sort] = sort_column
+    end
+
+    if redirect
+      flash.keep
+      redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings])
+    else
+      @all_ratings = Movie.ratings
+      @movies = Movie.find_by_ratings_and_order params[:ratings].keys, sort_column
+
+    end
   end
 
   def new
@@ -19,7 +38,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create!(movie_params)
+    @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -30,7 +49,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
+    @movie.update_attributes!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
@@ -42,4 +61,23 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  private
+
+  def ratings_filter
+    if params[:ratings]
+       params[:ratings]
+    elsif !session[:ratings]
+       Hash[Movie.ratings.map { |x| [x, 1] }]
+    else
+       session[:ratings]
+    end
+  end
+
+  def sort_column
+     Movie.column_names.include?(params[:sort]) ? params[:sort] : session[:sort]
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  "asc" : "desc"
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+  def self.ratings
+    %w(G PG PG-13 R)
+  end
+
+  def self.find_by_ratings_and_order(ratings, order_by_column)
+    movies = Movie.all
+    movies = movies.where(:rating => ratings)
+    movies = movies.order(order_by_column) if order_by_column.present?
+    movies
+  end
+
 end
